fee35ff7449506d6007fa080286daf210db5819d
Igor Shargin <igor20513@gmail.com>
2024-04-28 19:40:59 +0500

    fix: button style path

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,6 +1,6 @@
 import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
-import s from 'src/components/ui/button/button.module.scss'
+import s from './button.module.scss'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
