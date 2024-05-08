58dc8179ab59ca7912190be72fe2bdb5414505f3
Igor Shargin <igor20513@gmail.com>
2024-05-09 00:43:14 +0500

    fix(Button): Add forwardRef

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,8 +1,16 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import {
+  ComponentPropsWithRef,
+  ComponentPropsWithoutRef,
+  ElementType,
+  ReactNode,
+  forwardRef,
+} from 'react'
 
 import { clsx } from 'clsx'
 
 import s from './button.module.scss'
+
+export type PolymorphicRef<T extends ElementType = 'button'> = ComponentPropsWithRef<T>['ref']
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
@@ -12,13 +20,16 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   variant?: 'link' | 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
-export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  <T extends ElementType = 'button'>(props: ButtonProps<T>, ref?: PolymorphicRef<T>) => {
+    const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
 
-  return (
-    <Component
-      className={clsx(s.button, s[variant], fullWidth && s.fullWidth, className)}
-      {...rest}
-    />
-  )
-}
+    return (
+      <Component
+        className={clsx(s.button, s[variant], fullWidth && s.fullWidth, className)}
+        ref={ref}
+        {...rest}
+      />
+    )
+  }
+)
