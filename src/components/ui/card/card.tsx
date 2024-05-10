57e4db7a9e4b76a14917b039bdf31c913f153c99
Igor Shargin <igor20513@gmail.com>
2024-05-10 19:40:30 +0500

    fix(Card): add forwardRef

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,16 +1,11 @@
-import { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
-import s from './card.module.scss'
+import { clsx } from 'clsx'
 
-export type CardProps = {
-  children: ReactNode
-  className?: string
-} & ComponentPropsWithoutRef<'div'>
+import styles from './card.module.scss'
 
-export const Card = ({ children, className, ...rest }: CardProps) => {
-  return (
-    <div className={`${s.card} ${className}`} {...rest}>
-      {children}
-    </div>
-  )
-}
+export type CardProps = ComponentPropsWithoutRef<'div'>
+
+export const Card = forwardRef<ElementRef<'div'>, CardProps>(({ className, ...rest }, ref) => {
+  return <div className={clsx(styles.card, className)} ref={ref} {...rest} />
+})
